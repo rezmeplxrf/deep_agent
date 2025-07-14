668a3db96f9b0ad828f131b032885b61c1841543
Donghu <rezmeplx@rfway.org>
2025-07-14 15:12:18 +0900

    init

---
+++ lib/deep_agent.dart
@@ -0,0 +1,10 @@
+/// deep_agent, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate deep_agent
+/// dart pub global activate deep_agent
+///
+/// # see usage
+/// deep_agent --help
+/// ```
+library;

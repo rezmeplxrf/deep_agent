8bf6d368c037ba3c7adb24baa05ffecc0ee2584f
Donghu <rezmeplx@rfway.org>
2025-07-18 01:09:38 +0900

    Refactor workflows and service: update prompt for test2 and change leet code question to medium level; remove two_sum_test.dart

--- lib/src/features/workflow/service.dart
+++ lib/src/features/workflow/service.dart
@@ -8,7 +8,7 @@ import 'package:process/process.dart';
 void main() async {
   final service = WorkflowService();
   final results = await service.run(
-    'Write a easy level leet code question in `./test/` directory with the efficient solution.',
+    'Write a medium level leet code question in `./test/` directory with the efficient solution. - except to the two-sum question',
   );
   print(results);
 }
